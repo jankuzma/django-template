f33e8b6f74f6b0bc53c11020e55a446429683ea0
Jan Kuźma <134098590+jankuzma@users.noreply.github.com>
2023-08-16 16:41:35 +0200

    configured secret key environ variable

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, 'env', '.env'))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*rdkphloss%$%w2&-b5lrieppga)81%gj8+wkbavea&m=bh%42'
+SECRET_KEY = os.environ.get('DJ_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
