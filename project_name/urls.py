66a3776ae70d30bace7643369d34d9a2f602ecd8
Jan Kuźma <134098590+jankuzma@users.noreply.github.com>
2023-08-18 17:54:51 +0200

    create template for django-admin command with --template arg

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,4 +1,4 @@
-"""config URL Configuration
+"""{{ project_name }} URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -17,7 +17,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from config import settings
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
